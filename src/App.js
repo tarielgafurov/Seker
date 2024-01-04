699825b7240928ce98767d9106c8cb9c0b72db59
Kadyrbaev <sadyr_9494@mail.ru>
2024-01-04 21:43:59 +0600

    delete comp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-import Button from './components/UI/Button';
-import ImagePicker from './components/UI/ImagePicker';
-import Input from './components/UI/Input';
 
 function App() {
   return (
     <div className="App">
-      <ImagePicker/>
     </div>
   );
 }
