d0dc89020c744e07cb723a15ac3dd30a6a0cdd57
tarielgafurov <tarielgafurov618@gmail.com>
2024-01-07 14:45:00 +0300

    new tasks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Header from './layout/Header';
+import MainImageSlider from './layout/MainImageSlider';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <MainImageSlider/>
     </div>
   );
 }
